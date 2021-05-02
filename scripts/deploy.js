fad4b9b5145819eac085d113dfe49c2c853f1d74
Chris Chua <chris.sirhc@gmail.com>
2021-05-01 23:39:50 -0700

    add a simple deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const INITAL_SUPPLY = 100;
+
+async function main() {
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
+  
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const ProjectToken = await ethers.getContractFactory("ProjectToken");
+  const token = await ProjectToken.deploy(INITAL_SUPPLY);
+
+  console.log("Token address:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
