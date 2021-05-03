4e9dc8514d59c7f8b7e3229c90b45ddab8396ad6
Chris Chua <chris.sirhc@gmail.com>
2021-05-02 18:02:16 -0700

    add etherscan to see if we can get verify script working

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
+import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
+import {ALCHEMY_API_KEY, ROPSTEN_PRIVATE_KEY, ETHERSCAN_API_KEY} from "./secrets.js";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.1",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
